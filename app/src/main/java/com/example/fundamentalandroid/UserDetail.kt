bde0398fdb5bd425f67d4f0f2182552de78d7e58
Muhammad Taufik Hidayat <79823539+Taufik322@users.noreply.github.com>
2022-03-15 13:18:50 +0800

    add viewBinding

--- app/src/main/java/com/example/fundamentalandroid/UserDetail.kt
+++ app/src/main/java/com/example/fundamentalandroid/UserDetail.kt
@@ -1,13 +1,15 @@
 package com.example.fundamentalandroid
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.example.fundamentalandroid.databinding.ActivityUserDetailConstraintBinding
 
 class UserDetail : AppCompatActivity() {
+
+    private lateinit var binding : ActivityUserDetailConstraintBinding
 
     companion object {
         const val EXTRA_USER_IDENTITY = "extra_user_identity"
@@ -15,26 +17,16 @@ class UserDetail : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_detail)
-
-        val tvDataUnameReceived: TextView = findViewById(R.id.tv_detail_username)
-        val tvDataNameReceived: TextView = findViewById(R.id.tv_detail_name)
-        val tvDataRepositoryReceived: TextView = findViewById(R.id.tv_detail_repository)
-        val tvDataProfilePicture: ImageView = findViewById(R.id.im_detail_profile_picture)
-        val tvDataFollowers: TextView = findViewById(R.id.tv_detail_followers)
-        val tvDataFollowing: TextView = findViewById(R.id.tv_detail_following)
-        val tvDataLocation: TextView = findViewById(R.id.tv_detail_location)
-        val tvDataCompany: TextView = findViewById(R.id.tv_detail_company)
+        binding = ActivityUserDetailConstraintBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val userIdentity = intent.getParcelableExtra<User>(EXTRA_USER_IDENTITY) as User
-        tvDataUnameReceived.text = userIdentity.username
-        tvDataNameReceived.text = userIdentity.name
-        tvDataRepositoryReceived.text = "${userIdentity.repository} Repository"
-        Glide.with(this).load(userIdentity.profilePicture).circleCrop().into(tvDataProfilePicture)
-        tvDataFollowers.text = userIdentity.followers
-        tvDataFollowing.text = userIdentity.following
-        tvDataLocation.text = userIdentity.location
-        tvDataCompany.text = userIdentity.company
+        binding.tvDetailUsername.text = userIdentity.username
+        binding.tvDetailName.text = userIdentity.name
+        Glide.with(this).load(userIdentity.profilePicture).circleCrop().into(binding.imDetailProfilePicture)
+        binding.tvDetailFollowers.text = userIdentity.followers
+        binding.tvDetailFollowing.text = userIdentity.following
+        binding.tvDetailLocation.text = userIdentity.location
 
         supportActionBar?.title = userIdentity.name
     }
